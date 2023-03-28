36ed9d26c6e236e743ff76b0b9958f227025fbeb
Grigoriy Lyalko <98054846+Greg2089@users.noreply.github.com>
2023-03-28 10:09:49 +0300

    2) Код для GameFragment.kt

--- app/src/main/java/com/hfad/guessinggame/GameFragment.kt
+++ app/src/main/java/com/hfad/guessinggame/GameFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.hfad.guessinggame.databinding.FragmentGameBinding
 
 
@@ -17,13 +18,99 @@ class GameFragment : Fragment() {
     private val binding get() = _binding!!
 
     val words = listOf("Android", "Activity", "Fragment") //доступные для угадывания слова
+    val secretWord = words.random().uppercase()//слово, которое пользователь должен угадать
+    var secretWordDisplay = ""// как отображается это слово
+    var correctGuesses = ""          // правильно
+    var incorrectGuesses = ""       // неправильно
+    var livesLeft = 8                   // жизни
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        /*Свойству _binding присваивается экземпляр FragmentOrderBinding в методе onCreateView()*/
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        val view = binding.root
+        // определите, как должно отобрадаться секретное слово и обновите экран
+        secretWordDisplay = deriveSecretWordDisplay()
+        updateScreen()
 
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding.guessButton.setOnClickListener {
+            //Вызовите makeGuess, чтобы разобраться с предположениями пользователя
+            makeGuess(binding.guess.text.toString().uppercase())
+            binding.guess.text = null  // сбросить текст редактирования
+            updateScreen()  // обновить экран
+            // если пользователь выиграл или проиграл перейдите к ResultFragment передав значение сообщения
+            if (isWon() || isLost()) {
+                val action =
+                    GameFragmentDirections.actionGameFragmentToResultFragment(wonLostMessage())
+                view.findNavController().navigate(action)
+            }
+
+        }
+        return view
+
+    }
+
+    /* Когда фрагмент больше не имеет доступа к своему макету, установите для свойства _binding значение null*/
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    // установите TextView макета
+    fun updateScreen() {
+        binding.word.text = secretWordDisplay
+        binding.lives.text = "У Вас осталось $livesLeft жизней. "
+        binding.incorrectGuesses.text = "Неверных догадок $incorrectGuesses"
+    }
+
+    // Эта функция создает строку для того, как секретное слово должно отображаться на экране
+    fun deriveSecretWordDisplay(): String {
+        var display = ""
+        secretWord.forEach {
+            display += checkLetter(it.toString()) /*Вызовите checkLetter для каждой буквы в секретном слове
+        и добавьте ее возвращаемое значение в конец отображаемой переменной*/
+
+        }
+        return display
+    }
+
+    /*Эта функция проверяет, содержит ли секретное слово букву, которую угадал пользователь,
+    если да, то возвращает букву. Если нет, то он возвращает "_"*/
+    fun checkLetter(str: String) = when (correctGuesses.contains(str)) {
+        true -> str
+        false -> "_"
+    }
+
+    fun makeGuess(guess: String) {//Эта функция вызывается каждый раз, когда пользователь делает предположение
+        if (guess.length == 1) {
+            /*Для каждого правильного предположения обновляйте
+             правильные догадки и отображайте секретное слово*/
+            if (secretWord.contains(guess)) {
+
+                correctGuesses += guess
+                secretWordDisplay = deriveSecretWordDisplay()
+                //За каждое неверное предположение обновляйте неверные догадки и жизни
+            } else {
+                incorrectGuesses += "$guess"
+                livesLeft--
+            }
+        }
+    }
+
+    //Игра считается выигранной, если секретное слово соответствует отображаемому секретному слову
+    fun isLost() = livesLeft <= 0
+
+    //игра проиграна, если у пользователя кончились жизни
+    fun isWon() = secretWord.equals(secretWordDisplay, true)
+    fun wonLostMessage(): String {
+        var message = ""
+        if (isWon()) message = "Выиграл!"
+        else if (isLost()) message = "Проиграл!"
+        message += "Было загадано $secretWord."
+        return message //wonLostMessage() возвращает строку, выиграл ли пользователь и каким было секретное слово
     }
 
 }
